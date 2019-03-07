7842d364c36477beb86f9402ef03d0cc81cb86e3
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2019-03-07 18:44:52 +0900

    DB changed

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const knex = require('knex');
 module.exports = knex({
   client: 'pg',
   connection: {
-    host: 'kwangilmes.ccbl9zbqcka3.ap-northeast-2.rds.amazonaws.com',
-    user: 'kwangilmes',
-    password: 'rhkddlfvmfktmxlr',
-    database: 'kwangilmes2'
+    host: 'kwdevdb.ccbl9zbqcka3.ap-northeast-2.rds.amazonaws.com',
+    user: 'kwanwooi',
+    password: 'rhksnsla12',
+    database: 'kwangilmes'
   }
 });
 
